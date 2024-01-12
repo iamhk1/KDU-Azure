bb08674dba0950d4c2e86181b1846c5cc3e71354
Harsh Kachhawa <harsh.kachhawa2002@gmail.com>
2024-01-12 22:47:21 +0530

    Azure project conpleted

---
+++ src/main/java/com/kdu/assignment/AssignmentApplication.java
@@ -0,0 +1,23 @@
+package com.kdu.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		public String index() {
+
+			return "Hello,Harsh Kachhawa KD Batch-24<Azure>!";
+		}
+	}
+
+}
